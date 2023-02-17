3f39f3fbc1ae4ca9a138293f8331b83e3d779197
Mikhail <mihail.zharavin65@gmail.com>
2023-02-17 18:53:31 +0200

    Installed MUI

--- src/App.tsx
+++ src/App.tsx
@@ -4,40 +4,8 @@ import "./App.css";
 import { useQuery } from "@apollo/client";
 import { gql } from "./generated/gql";
 
-function App() {
-  const GET_CHARACTERS = gql(/* GraphQL */ `
-    query ExampleQuery {
-      characters {
-        results {
-          name
-          id
-          image
-        }
-      }
-    }
-  `);
-  const { data, loading, error } = useQuery(GET_CHARACTERS);
-
-  if (loading) {
-    return <>LOADING....</>;
-  }
-
-  if (error) {
-    return <>ERROR!!!</>;
-  }
-
-  return (
-    <div className="App">
-      {data &&
-        data.characters?.results?.map((characters) => {
-          return (
-            <div key={characters?.id} className="wrapper">
-              {characters?.name}
-            </div>
-          );
-        })}
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
