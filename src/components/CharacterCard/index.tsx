a2853181c7c7b71659bb56c23fa73a3633cca599
Mikhail <mihail.zharavin65@gmail.com>
2023-03-05 12:49:16 +0200

    Added React memo to CharacterCard
    Added React memo to CharacterCard

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -6,6 +6,7 @@ import {
   Link,
   Typography,
 } from "@mui/material";
+import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 
 type CharacterProps = {
@@ -15,27 +16,25 @@ type CharacterProps = {
   created: string;
 };
 
-export const CharacterCard: React.FC<CharacterProps> = ({
-  name,
-  image,
-  id,
-  created,
-}) => {
-  return (
-    <Card sx={{ maxWidth: 345, margin: "0 auto" }}>
-      <CardActionArea>
-        <Link underline="none" component={RouterLink} to={`character/${id}`}>
-          <CardMedia sx={{ height: 300 }} image={image} title={name} />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {name}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {new Date(created).toLocaleDateString()}
-            </Typography>
-          </CardContent>
-        </Link>
-      </CardActionArea>
-    </Card>
-  );
-};
+export const CharacterCard: React.FC<CharacterProps> = React.memo(
+  ({ name, image, id, created }) => {
+    console.log("characted card render");
+    return (
+      <Card sx={{ maxWidth: 345, margin: "0 auto" }}>
+        <CardActionArea>
+          <Link underline="none" component={RouterLink} to={`character/${id}`}>
+            <CardMedia sx={{ height: 300 }} image={image} title={name} />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {name}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {new Date(created).toLocaleDateString()}
+              </Typography>
+            </CardContent>
+          </Link>
+        </CardActionArea>
+      </Card>
+    );
+  }
+);
